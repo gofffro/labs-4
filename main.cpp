7cf2021d304075d665904c6683d7632a978f7fff
gofffro <vazheninstepan@gmail.com>
2024-09-27 19:54:58 +0700

    Добавлен main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    const double v = 0.009;
+    const double d = 1.91;
+    const double x = 9.2 * pow(10,5);
+
+    double L;
+    double R;
+
+    int a[7] {100,  200,  500,  1000,  2000,  5000,  10000};
+
+    for (int i = 0; i < 7; ++i) {
+        R = (a[i] * d) / v;
+        if (R > x) {
+            L = 0.021 / pow(d, 0.3);
+        } else {
+            L = pow((((1.5 * pow(10, -6)) / d) + (1 / R)), 0.3);
+        }
+        cout << ::fixed << R;
+        cout << " " << L << endl;
+    }
+}
